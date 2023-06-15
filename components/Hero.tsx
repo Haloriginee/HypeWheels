99b45e281eb1219597cea7aff0d325fe2cf42be0
Kevin Hang <herezy2006@gmail.com>
2023-06-15 15:30:18 +0200

    hero section ok

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -26,6 +26,16 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
+      <div className="hero__image-container">
+        <div className="hero__image">
+          <Image
+            src="/hero.png"
+            alt="hero"
+            fill className="object-contain"
+          />
+          <div className="hero__image-overlay"/>
+        </div>
+      </div>
     </div>
   )
 }
