086854f45b1c32e7d7583469d1be546b1653cfd2
Kevin Hang <herezy2006@gmail.com>
2023-06-15 19:43:39 +0200

    front-end done-ish

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -33,8 +33,8 @@ const Hero = () => {
             alt="hero"
             fill className="object-contain"
           />
-          <div className="hero__image-overlay"/>
         </div>
+        <div className="hero__image-overlay"/>
       </div>
     </div>
   )
