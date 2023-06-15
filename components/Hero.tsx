20cc87538b2bc491a905f36fa344391dd5ac8172
Kevin Hang <herezy2006@gmail.com>
2023-06-15 12:23:16 +0200

    created button + button props

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,32 @@
+"use client"; // make it client side
+
 import Image from "next/image";
+import CustomButton from "./CustomButton";
 
 const Hero = () => {
+
+  const handleScroll = () => {
+
+  }
+
   return (
-    <div>Hero</div>
+    <div className="hero">
+      <div className="flex-1 pt-36 padding-x">
+        <h1 className="hero__title">
+          Unlock Your Journey: Explore, Rent, and Go!
+        </h1>
+
+        <p className="hero__subtitle">
+          Rev Up the Adventure: Hit the Road with Unmatched Freedom and Flexibility!
+        </p>
+
+        <CustomButton
+          title="Explore"
+          containerStyles="bg-teal-500 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded"
+          handleClick={handleScroll}
+        />
+      </div>
+    </div>
   )
 }
 
